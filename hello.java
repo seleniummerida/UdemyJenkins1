ea61e102aef9bfcb1a2fd2e47dbb15cdb318f098
seleniummerida <selenium.merida@gmail.com>
2020-05-08 23:56:54 -0500

    first change

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@ public class hello{
 	public static void main(String[] args){
 		for(int i=0; i<10; i++)
 		{
-			System.out.println("Hello from " + i);
+			System.out.println("Hello from " + i + "Vato Loco");
 		}
 	}
 }
